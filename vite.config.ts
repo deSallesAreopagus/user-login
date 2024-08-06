f4ddc0f397a489eaccffb1248582b636c8543fe5
rafaellevissa <rafaellevi.costa@e-stracta.com>
2024-08-06 16:59:22 -0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'microfrontend2',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App.tsx'
+      }
+    })
+  ],
+  build: {
+    target: 'esnext',
+    minify: false
+  },
+  server: {
+    port: 3002
+  }
+})
